7ea18ae9dceebede91da4463de61fb8726f181dd
Zhenglin Huang <waltyellow@users.noreply.github.com>
2015-03-19 02:26:27 -0400

    Update 318plc.c

--- 318plc.c
+++ 318plc.c
@@ -1,4 +1,25 @@
 #include <stdio.h> /*where printf,scanf are */
 #include <stdkub.h> /*where malloc, calloc and free are */
 
-void
+//c has to know a value
+void printarray(int a[][5], int numrows){
+  int i,j;
+  
+  printf("[\n");
+  for (i = 0; i< numrows;i++){
+   for (j = 0; j< ???; j++){
+     printf("%2d ", a[i][j]);
+   }
+   printf("]\n");
+  }
+  printf("]\n");
+}
+
+int main(int numArguments, char *arguments[]){
+  int i;
+  int a[][5] = {{5,6,7,8,9},{15,16,17,18,19},{26,27};
+  //what if it does not have 5 for the third one? it allocates 0
+  printf("Hello world \n");
+  printarray(a, 3);
+  
+}
